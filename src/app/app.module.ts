e943916eed9b2499fbd04a2a025f00a9cdeb2545
hodkyy@gmail.com <Hodky1597>
2023-10-04 16:11:56 +0200

    F-2 doplnenie headeru a footeru do app componentu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './components/example/example.component';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ExampleComponent } from './components/example/example.component';
     ExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
