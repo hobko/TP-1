b2debfb886468171724b2a8fbf25664de9332b3b
hodkyy@gmail.com <Hodky1597>
2024-01-31 00:14:44 +0100

    pridanie funkcnosti na popup nahravanie

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { FileUploader } from 'ng2-file-upload';
+import { FileUploader, FileItem, ParsedResponseHeaders } from 'ng2-file-upload';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from '../../services/api.service';
 import { environment } from '../../../environments/environment';
-
+import { endpoints } from '../../../environments/endpoints';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css']
-
 })
 export class HelloComponent implements OnInit {
-  private apiUrl = environment.apiUrl
   message: string = '';
+  private apiUrl = environment.apiUrl;
   uploader: FileUploader = new FileUploader({ url: `${this.apiUrl}upload` });
 
-  constructor(private http: HttpClient) {}
+  constructor(private apiService: ApiService, private toastr: ToastrService) {}
 
   ngOnInit() {
-    this.http.get<any>(`${this.apiUrl}hello`).subscribe((data) => {
-      this.message = data.message;
-    });
+    this.apiService.getHello().subscribe(
+      (data) => {
+        this.message = data.message;
+      }
+    );
 
     this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false;
+    };
+
+    // Handle completion of each file upload
+    this.uploader.onCompleteItem = (item: FileItem, response: string, status: number, headers: ParsedResponseHeaders) => {
+      if (status === 200 || status === 500) {
+        const jsonResponse = JSON.parse(response);
+        this.toastr.success('Súbor sa nahral úspešne', 'Potvrdenie');
+      }
     };
   }
 
