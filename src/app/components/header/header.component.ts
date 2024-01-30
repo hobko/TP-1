34d8ce66f141bf9f1d473a1b9ae7f5710cfab6c0
xvargam5 <matkovarga10@gmail.com>
2024-01-30 19:57:39 +0100

    Skusobna verzia

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
- 
+export class HeaderComponent implements OnInit{
+  private apiUrl = environment.apiUrl
+  message: string = '';
+  isSystemUp: boolean = false;
+  uploader: FileUploader = new FileUploader({ url: `${this.apiUrl}upload` });
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>(`${this.apiUrl}hello`).subscribe((data) => {
+      this.message = data.message;
+      this.isSystemUp = this.message === "System is up";
+    });
+  }
+
+
+
 }
+
+
