b1d577829ffcbba8c15afedec3b8ff415d560bd0
hodkyy@gmail.com <Hodky1597>
2024-02-12 19:51:56 +0100

    Pridanie delete funkcionality

--- src/environments/endpoints.ts
+++ src/environments/endpoints.ts
@@ -7,5 +7,6 @@ export const endpoints = {
     apiGetgpxMatched:           environment.apiUrl + 'getgpx/matched', // tu sa este dava /nazov_suboru
     apiGetFiles:                environment.apiUrl + 'getfiles',
     apiGetMapMatchingStatus:    environment.apiUrl + 'graphhopper/status',
-    apiDownloadZipFile:         environment.apiUrl + 'download' // tu sa este dava /nazov_suboru
+    apiDownloadZipFile:         environment.apiUrl + 'download', // tu sa este dava /nazov_suboru
+    apiDeleteFile:              environment.apiUrl + 'delete/storage' // tu sa este dava /nazov_suboru
 }
