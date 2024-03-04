f8f8df284c16a7878f4562ced24c47c334d41630
hodkyy@gmail.com <Hodky1597>
2024-03-04 22:50:25 +0100

    funguje to ta tabulka

--- src/app/components/map-option/map-option.component.ts
+++ src/app/components/map-option/map-option.component.ts
@@ -1,7 +1,7 @@
+// Inside your component class (map-option.component.ts)
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { FileService } from '../../services/file.service';
 import { NotificationService } from 'src/app/services/notification.service';
-
 
 @Component({
   selector: 'app-map-option',
@@ -11,17 +11,30 @@ import { NotificationService } from 'src/app/services/notification.service';
 export class MapOptionComponent {
   @Output() optionClicked = new EventEmitter<{ option: string, name: string }>();
   files: string[] = [];
+  upload_files: { name: string, selected: boolean }[] = [];
+  selectAllCheckbox: boolean = false; // Property to bind to the checkbox in the thead
+  selectAll = false;
 
   constructor(private fileService: FileService,
               private notificationService: NotificationService,) {}
 
   ngOnInit(): void {
     this.loadFiles();
+    this.loadUploadFiles();
     this.fileService.filesUpdated$.subscribe(() => {
       this.loadFiles();
     });
   }
 
+  loadUploadFiles(): void {
+    this.fileService.getUploadsFiles().subscribe(response => {
+      if (response && response.files) {
+        this.upload_files = response.files.map(file => ({ name: file, selected: false }));
+      } else {
+        this.upload_files = []; 
+      }
+    });
+  }
   loadFiles(): void {
     this.fileService.getFiles().subscribe(response => {
       this.files = response.files;
@@ -33,6 +46,7 @@ export class MapOptionComponent {
     const name = option.split(' - ')[0]; // Extracting the name from the option
     this.optionClicked.emit({ option, name });
   }
+
   onDeleteClick(event: MouseEvent, file: string): void {
     event.stopPropagation(); // Prevent the click event from bubbling up to the parent li element
     this.fileService.deleteDataFromStorage(file);
@@ -44,4 +58,36 @@ export class MapOptionComponent {
     this.fileService.downloadGpxZipFile(file);
   }
 
+  toggleSelectAll(): void {
+    // Set the selected property of each upload_file object based on the value of selectAllCheckbox
+    this.upload_files.forEach(file => file.selected = this.selectAllCheckbox);
+  }
+
+  deleteSelectedFiles(): void {
+    const selectedFiles = this.upload_files.filter(file => file.selected).map(file => file.name);
+    this.fileService.deleteFilesFromUploads(selectedFiles).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.error('Error:', error); 
+      }
+    );
+  }
+
+  convertSelectedFiles(): void{
+    const selectedFiles = this.upload_files.filter(file => file.selected).map(file => file.name);
+    this.fileService.convertFilesFromUploads(selectedFiles).subscribe(
+      response => {
+        this.notificationService.showSuccess("Súbory sa nahrali úspešne","Hotovo");
+        this.fileService.notifyFilesUpdated();
+        this.loadUploadFiles();
+      },
+      error => {
+        this.notificationService.showError("Nastala chyba","Chyba");
+
+      }
+    );
+
+  }
 }
