445a8f9d0ba11b0912862522638c404a8ba50cfd
LiquiNaut <boetfly@gmail.com>
2024-03-20 12:53:46 +0100

    messages rewrite

---
+++ src/app/messages/info-messages.ts
@@ -0,0 +1,11 @@
+export const InfoMessages = {
+  welcomeMessage: {
+    message: "Vitajte v našej aplikácii. Tešíme sa na vašu spoluprácu.",
+    title: "Vitajte"
+  },
+  sessionExtended: {
+    message: "Vaša relácia bola predĺžená o ďalších 30 minút.",
+    title: "Relácia Predĺžená"
+  }
+  // Ďalšie definície informačných hlásení...
+};
